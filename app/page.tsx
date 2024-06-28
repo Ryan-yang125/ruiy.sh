6aca5b783f6e8010910a5397b1a88faad2d0fdd0
Ryan-yang125 <ruiyang0012@gmail.com>
2024-06-28 17:14:55 +0800

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Rui Yang.
       </h1>
-      <p className="mb-4">{`Full stack dev. WeChat to Web3.`}</p>
+      <p className="mb-4">{`Full stack dev. ex-WeChat.`}</p>
     </section>
   );
 }
